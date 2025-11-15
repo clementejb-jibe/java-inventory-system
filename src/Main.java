11c49a35d7109ce96e043fd4fce58a568d3b1a16
jbbb <clementejb3@gmail.com>
2025-11-15 15:26:42 +0800

    Added Repository -update addProduct() method's logic

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import model.Product;
+import repository.Repository;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* ---------------
+        For Debugging Only
+            -Method Testing
+          ---------------*/
+
+        /*
+        Scanner scan =new Scanner(System.in);
+        Product product = new Product();
+        Repository db = new Repository(product, scan);
+
+        db.addProduct(product);
+         */
+    }
+}
